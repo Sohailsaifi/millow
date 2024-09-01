62a01ab3537d634e2a482e1084938ab4b5cc1c65
Sohailsaifi <sohailsaifi65@gmail.com>
2024-09-01 13:56:50 +0530

    Starter code for the project

---
+++ contracts/Escrow.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+interface IERC721 {
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id
+    ) external;
+}
+
+contract Escrow {
+    address public inspector;
+    address public lender;
+}
